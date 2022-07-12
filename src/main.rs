ff6ffaba1e1b1ef08ffe1c1002e0b63ff5762ab5
sugardaddy <liamlangli@tencent.com>
2022-07-12 17:00:38 +0800

    add v8

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,29 @@ use winit::{
     window::WindowBuilder,
 };
 
+use v8;
+
 fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let platform = v8::new_default_platform(0, false).make_shared();
+    v8::V8::initialize_platform(platform);
+    v8::V8::initialize();
+    
+    let isolate = &mut v8::Isolate::new(Default::default());
+    
+    let scope = &mut v8::HandleScope::new(isolate);
+    let context = v8::Context::new(scope);
+    let scope = &mut v8::ContextScope::new(scope, context);
+    
+    let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
+    println!("javascript code: {}", code.to_rust_string_lossy(scope));
+    
+    let script = v8::Script::compile(scope, code, None).unwrap();
+    let result = script.run(scope).unwrap();
+    let result = result.to_string(scope).unwrap();
+    println!("result: {}", result.to_rust_string_lossy(scope));
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
