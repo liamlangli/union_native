40f18b3c98d75f9f9846e3af434d997d2f1a1780
lang <lilang8936@gmail.com>
2023-06-05 16:24:22 +0800

    add glad

--- src/foundation/linux/linux_window.c
+++ src/foundation/linux/linux_window.c
@@ -2,12 +2,17 @@
 
 #ifdef OS_LINUX
 
+#include "glad/gles2.h"
 #define GLFW_INCLUDE_ES32 
 #include "GLFW/glfw3.h"
+
 window_t* platform_window_create(const char* title, int width, int height)
 {
     glfwInit();
     // glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
     GLFWwindow* native_window = glfwCreateWindow(width, height, title, NULL, NULL);
     window_t* window = malloc(sizeof(window_t));
@@ -18,6 +23,13 @@ window_t* platform_window_create(const char* title, int width, int height)
 
     glfwMakeContextCurrent(native_window);
     glfwSwapInterval(1);
+
+    int version = gladLoadGLES2(glfwGetProcAddress);
+    if (version == 0) {
+        printf("Failed to initialize OpenGL context\n");
+        exit(-1);
+    }
+    glClearColor(1.0, 1.0, 1.0, 1.0);
     return window;
 }
 
@@ -25,6 +37,7 @@ void platform_window_update(window_t* window)
 {
     while (!glfwWindowShouldClose(window->native_handle))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window->native_handle);
         glfwPollEvents();
     }
@@ -33,6 +46,7 @@ void platform_window_update(window_t* window)
 void platform_window_destroy(window_t* window)
 {
     glfwDestroyWindow(window->native_handle);
+    glfwTerminate();
     free(window);
 }
 
