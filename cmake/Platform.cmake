a6f49760c0e42f631dc7fdb974c1c6aa4c4ad397
lilang.liam <lilang.liam@bytedance.com>
2024-06-18 14:55:31 +0800

    chore: Update CMakeLists.txt and Platform.cmake to support macOS SDK path retrieval

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -4,6 +4,15 @@ set(USER_AGENT "union_native/${VERSION} ${CMAKE_SYSTEM_NAME}")
 message(STATUS "USER_AGENT: ${USER_AGENT}")
 add_definitions(-DUSER_AGENT="${USER_AGENT}")
 
+if(APPLE)
+    execute_process(
+        COMMAND xcrun --sdk macosx --show-sdk-path
+        OUTPUT_VARIABLE CMAKE_OSX_SYSROOT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    message(STATUS "CMAKE_OSX_SYSROOT: ${CMAKE_OSX_SYSROOT}")
+endif()
+
 set(PROC_ARCH x86_64)
 if (CMAKE_SYSTEM_PROCESSOR MATCHES "arm64")
     set(PROC_ARCH arm64)
