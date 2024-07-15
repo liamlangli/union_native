52df5a35b84859b70457d7f54eeab4a192904813
lang <lilang8936@gmail.com>
2024-07-16 01:01:06 +0800

    Update bundle identifier to com.liamlangli.un

--- cmake/PostBuild.cmake
+++ cmake/PostBuild.cmake
@@ -1,3 +1,7 @@
+macro (set_xcode_property TARGET XCODE_PROPERTY XCODE_VALUE)
+    set_property (TARGET ${TARGET} PROPERTY XCODE_ATTRIBUTE_${XCODE_PROPERTY} ${XCODE_VALUE})
+endmacro (set_xcode_property)
+
 if (APPLE)
     add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_directory
@@ -5,15 +9,22 @@ if (APPLE)
     add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy
         ${CMAKE_SOURCE_DIR}/app/AppIcon.icns $<TARGET_FILE_DIR:${PROJECT_NAME}>/../Resources/AppIcon.icns)
+
+    if (IOS)
+        set(ENTITLEMENTS_FILE "${CMAKE_SOURCE_DIR}/app/un.entitlements")
+        set_xcode_property(${PROJECT_NAME} CODE_SIGN_IDENTITY "Apple Developer")
+        set_xcode_property(${PROJECT_NAME} DEVELOPMENT_TEAM "Lang Li (UNQ9CMR8W6)")
+        set_xcode_property(${PROJECT_NAME} PRODUCT_BUNDLE_IDENTIFIER "com.liamlangli.${PROJECT_NAME}")
+
+        add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+            COMMAND codesign --entitlements ${ENTITLEMENTS_FILE} --sign - $<TARGET_FILE:${PROJECT_NAME}>
+            COMMENT "Signing the application with entitlements"
+        )
+    endif()
 else()
     add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_directory
         ${CMAKE_SOURCE_DIR}/node/public $<TARGET_FILE_DIR:${PROJECT_NAME}>/public)
 endif()
 
-macro (set_xcode_property TARGET XCODE_PROPERTY XCODE_VALUE)
-    set_property (TARGET ${TARGET} PROPERTY XCODE_ATTRIBUTE_${XCODE_PROPERTY} ${XCODE_VALUE})
-endmacro (set_xcode_property)
 
-set_xcode_property(${PROJECT_NAME} CODE_SIGN_IDENTITY "iPhone Developer")
-set_xcode_property(${PROJECT_NAME} DEVELOPMENT_TEAM "angus lee (Personal Team)")
